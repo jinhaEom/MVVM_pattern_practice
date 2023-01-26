0e0a29ad80dea1456cb2862c5ed07ac1e4a35469
jinahaEom <umjin98@naver.com>
2023-01-26 15:58:35 +0900

    test

--- BookSearchApp/app/src/main/java/bu/ac/kr/booksearchapp/MainActivity.kt
+++ BookSearchApp/app/src/main/java/bu/ac/kr/booksearchapp/MainActivity.kt
@@ -1,11 +1,16 @@
 package bu.ac.kr.booksearchapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import bu.ac.kr.booksearchapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
---
+++ BookSearchApp/app/src/main/java/bu/ac/kr/booksearchapp/ui/view/FavoriteFragment.kt
@@ -0,0 +1,10 @@
+package bu.ac.kr.booksearchapp.ui.view
+
+import androidx.fragment.app.Fragment
+import bu.ac.kr.booksearchapp.databinding.FragmentFavoriteBinding
+
+class FavoriteFragment : Fragment() {
+    private var _binding: FragmentFavoriteBinding? = null
+    
+}
+
