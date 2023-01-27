7fe98c1b7c94a6c1567cbaa2239b71ae9dabd33b
jinahaEom <umjin98@naver.com>
2023-01-27 22:54:34 +0900

    retrofit instance 생성

---
+++ BookSearchApp/app/src/main/java/bu/ac/kr/booksearchapp/ui/view/MainActivity.kt
@@ -0,0 +1,50 @@
+package bu.ac.kr.booksearchapp.ui.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import bu.ac.kr.booksearchapp.R
+import bu.ac.kr.booksearchapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        setupBottomNavigationView()
+
+        if (savedInstanceState == null) {
+            binding.bottomNavigationView.selectedItemId = R.id.fragment_search
+            // 앱이 처음 실행되었을때 fragment_search 화면 띄움 
+        }
+    }
+
+    private fun setupBottomNavigationView() {
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.fragment_search -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frame_layout, SearchFragment())
+                        .commit()
+                    true
+                }
+                R.id.fragment_favorite -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frame_layout, FavoriteFragment())
+                        .commit()
+                    true
+                }
+                R.id.fragment_settings -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frame_layout, SettingFragment())
+                        .commit()
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+}
---
+++ BookSearchApp/app/src/main/java/bu/ac/kr/booksearchapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package bu.ac.kr.booksearchapp.util
+
+import bu.ac.kr.booksearchapp.BuildConfig
+
+object Constants {
+    const val BASE_URL = "https://dapi.kakao.com/"
+    const val API_KEY = BuildConfig.bookApiKey
+}
